e141df218e3b05767e8ab94592bc0b3bddf5880f
Steve Musgrave <steve.musgrave@yahoo.co.uk>
2014-04-02 16:07:26 +0100

    First letter method now working

--- directory.rb
+++ directory.rb
@@ -30,7 +30,9 @@ end
 
 def print(students)
 	students.each_with_index do |student, index|
-	puts " #{index+1} #{student[:name]} (#{student[:cohort]} cohort)"
+	if student[:name][0] =="J"
+		puts " #{index+1} #{student[:name]} (#{student[:cohort]} cohort)"
+	end
 	end
 end
 
