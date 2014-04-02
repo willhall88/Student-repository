30862c2c912c06e8c6b58efd08576dfe49094613
Steve Musgrave <steve.musgrave@yahoo.co.uk>
2014-04-02 12:17:44 +0100

    replace individual puts with a iterative loop

--- directory.rb
+++ directory.rb
@@ -25,27 +25,10 @@ students = [
 #no were going to print them
 puts "The students of my cohort at Makers Academy"
 puts "------------"
-puts students [0]
-puts students [1]
-puts students [2]
-puts students [3]
-puts students [4]
-puts students [5]
-puts students [6]
-puts students [7]
-puts students [8]
-puts students [9]
-puts students [10]
-puts students [11]
-puts students [12]
-puts students [13]
-puts students [14]
-puts students [15]
-puts students [16]
-puts students [17]
-puts students [18]
-puts students [19]
+students.each do |student|
+	puts student
+end
 
 # finally we print the total
-puts "Overall, we have #{student_count} great students"
+puts "Overall, we have #{students.length} great students"
 
