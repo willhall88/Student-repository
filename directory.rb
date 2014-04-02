2b96d40f9bc805379f1e33667aa9ec9012fd53ab
Will Hall <willhall88@hotmail.com>
2014-04-02 12:04:06 +0100

    I have put the students into an array

--- directory.rb
+++ directory.rb
@@ -1,30 +1,52 @@
 student_count = 20
-# first, we print the list of students
-puts "The Students of my cohort at Makers Academy"
-puts "---------"
-puts "Daniel Le Dosquet - Bergquist"
-puts "Steve Musgrave"
-puts "Sroop Sunar"
-puts "Colin Marshall"
-puts "Josh Fail-Brown"
-puts "Louise Lai"
-puts "Robin Doble"
-puts "Alex Wong"
-puts "Scott Dimmock"
-puts "Muhanad Al-Rubaiee"
-puts "Shelley Hope"
-puts "Will Hall"
-puts "Oliver Delevingne"
-puts "Nico Saueressig"
-puts "Apostolis"
-puts "Stefania Cardenas"
-puts "Robert Leon"
-puts "Emma Williams"
-puts "Joey Wolf"
-puts "Julie Walker"
+# Let's put all students into an array
+students = [
+"Daniel Le Dosquet - Bergquist",
+"Steve Musgrave",
+"Sroop Sunar",
+"Colin Marshall",
+"Josh Fail-Brown",
+"Louise Lai",
+"Robin Doble",
+"Alex Wong",
+"Scott Dimmock",
+"Muhanad Al-Rubaiee",
+"Shelley Hope",
+"Will Hall",
+"Oliver Delevingne",
+"Nico Saueressig",
+"Apostolis",
+"Stefania Cardenas",
+"Robert Leon",
+"Emma Williams",
+"Joey Wolf",
+"Julie Walker",
+]
+#no were going to print them
+puts "The students of my cohort at Makers Academy"
+puts "------------"
+puts students [0]
+puts students [1]
+puts students [2]
+puts students [3]
+puts students [4]
+puts students [5]
+puts students [6]
+puts students [7]
+puts students [8]
+puts students [9]
+puts students [10]
+puts students [11]
+puts students [12]
+puts students [13]
+puts students [14]
+puts students [15]
+puts students [16]
+puts students [17]
+puts students [18]
+puts students [19]
+puts students [20]
+
 # finally we print the total
-print "Overall, we have "
-# it's important that print() doesn't add new line characters
-print student_count
-puts " great students"
+puts "Overall, we have #{student_count} great students"
 
